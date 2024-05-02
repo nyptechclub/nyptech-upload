67266037a5b447862587b64cba1b7718647b8220
iamsven2005 <iamsven2005@gmail.com>
2024-05-02 13:40:54 +0800

    Favicon Change

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className="">
-      File Upload Service
+    <main className="hero min-h-screen bg-base-200">
+      <div className="hero-content text-center flex flex-col">
+
+      <img src="http://nyptech.vercel.app/favicon.ico"></img>
+      <div className="text-2xl font-bold">
+        File Upload Service
+      </div>
+      <Link href="/showcase" className="btn">Showcase</Link>
+      <Link href="/NYP_SOLVE" className="btn">NYP Solve</Link>
+      </div>
     </main>
   );
 }
